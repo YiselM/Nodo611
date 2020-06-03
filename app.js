af0fb790332d3db1daabadac1baa01d46252952f
Yisel Mesa <yiselm@uninorte.edu.co>
2020-06-02 22:30:06 -0500

    prueba

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ con.connect(function (err) {
 const quer1 = "SELECT P1, P2, P3, P4, P5, estado, CONCAT_WS(' ', fecha, hora) AS datetime FROM datos " +
     "WHERE CONCAT_WS(' ', fecha, hora) > date_sub(NOW(), INTERVAL 7 HOUR)";
 
-const querD = "SELECT P1, P2, P3, P4, P5, estado, CONCAT_WS(' ', fecha, hora) AS datetime FROM datos WHERE DATE(fecha) = CURDATE()";
-//"SELECT P1, P2, P3, P4, P5, estado, CONCAT_WS(' ', fecha, hora) AS datetime FROM datos WHERE CONCAT_WS(' ', fecha, hora) > DATE_SUB(NOW(), INTERVAL 15 HOUR)";
+const querD = "SELECT P1, P2, P3, P4, P5, estado, CONCAT_WS(' ', fecha, hora) AS datetime FROM datos WHERE DATE(fecha) = DATE_FORMAT(CONVERT_TZ(NOW(), 'GMT', 'America/Bogota'), '%Y-%m-%d')";
+//"SELECT P1, P2, P3, P4, P5, estado, CONCAT_WS(' ', fecha, hora) AS datetime FROM datos WHERE CONCAT_WS(' ', fecha, hora) > DATE_SUB(NOW(), INTERVAL 1 DAY)";
 
 const querS = "SELECT P1, P2, P3, P4, P5, estado, CONCAT_WS(' ', fecha, hora) AS datetime FROM datos WHERE  YEARWEEK(fecha, 1) = YEARWEEK(CURDATE(), 1)";
 //SELECT P1, P2, P3, P4, P5, estado, CONCAT_WS(' ', fecha, hora) AS datetime FROM datos WHERE YEARWEEK(fecha,1)=YEARWEEK(NOW(),1)-1";
